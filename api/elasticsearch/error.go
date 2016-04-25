085ccb45f4880b22c5a8cdae46dfd894c5d6fe1c
SNiko <SWashchyshchuk@gmail.com>
2016-04-25 16:52:51 +1000

    Fix elastic search error parsing

--- api/elasticsearch/error.go
+++ api/elasticsearch/error.go
@@ -1,7 +1,12 @@
 package elasticsearch
 
+import (
+	"net/http"
+	"fmt"
+)
+
 type Error struct {
-	Details ErrorDetails `json:"error"`
+	Details *ErrorDetails `json:"error"`
 	Status  int
 }
 
@@ -9,10 +14,14 @@ type ErrorDetails struct {
 	Reason    string
 	ErrorType string          `json:"type"`
 	RootCause []*ErrorDetails `json:"root_cause"`
-	CausedBy  *ErrorDetails   `json:"coused_by"`
+	CausedBy  *ErrorDetails   `json:"caused_by"`
 	Status    string
 }
 
 func (e Error) Error() string {
-	return e.Details.Reason
+	if e.Details != nil && e.Details.Reason != "" {
+		return fmt.Sprintf("Error %d (%s): %s [type=%s]", e.Status, http.StatusText(e.Status), e.Details.Reason, e.Details.ErrorType)
+	} else {
+		return fmt.Sprintf("Error %d (%s)", e.Status, http.StatusText(e.Status))
+	}
 }
