37ee61dd0ed5dca1758811f9f0b815894ce76a04
SNiko <SWashchyshchuk@gmail.com>
2016-05-01 11:07:01 +1000

    Add trip details to "flight" elastic search document
    The following changes has been done to the "fare" document:
    - rename document from "fare" to 'trip"
    - add trip_main_airline field which contains the name of the airline which is the most frequently used during the trip
    
    The following changes has been done to the "flihgt" document:
    - add trip_departure_date
    - add trip_origin_city
    - add trip_origin_coordinates
    - add trip_destination_city
    - add trip_destination_coordinates
    - add trip_main_airline
    
    This allows better filtering of documents regardless of their type document type. For example, flights filtered by a specific trip_main_airline will filter trips on the dashboard as well.

--- scanner/report_elastic.go
+++ scanner/report_elastic.go
@@ -15,20 +15,20 @@ const (
 	IndexConfig = `
 {
   "mappings": {
-    "fare": {
+    "trip": {
       "properties": {
-        "date": {
+        "trip_date": {
           "format": "strict_date_optional_time||epoch_millis",
           "type": "date"
         },
-        "day_of_the_week": {
+        "trip_day_of_the_week": {
           "type": "string",
           "index": "not_analyzed"
         },
         "fare_info": {
           "type": "string"
         },
-        "origin_city": {
+        "trip_origin_city": {
           "type": "string",
           "index": "not_analyzed"
         },
@@ -39,11 +39,15 @@ const (
           "type": "string",
           "index": "not_analyzed"
         },
-        "return_date": {
+        "trip_main_airline": {
+          "type": "string",
+          "index": "not_analyzed"
+        },
+        "trip_return_date": {
           "format": "strict_date_optional_time||epoch_millis",
           "type": "date"
         },
-        "return_day_of_the_week": {
+        "trip_return_day_of_the_week": {
           "type": "string",
           "index": "not_analyzed"
         },
@@ -55,14 +59,14 @@ const (
           "type": "string",
           "index": "not_analyzed"
         },
-        "destination_city": {
+        "trip_destination_city": {
           "type": "string",
           "index": "not_analyzed"
         },
-     	"origin_coordinates": {
+     	"trip_origin_coordinates": {
           "type": "geo_point"
         },
-        "destination_coordinates": {
+        "trip_destination_coordinates": {
           "type": "geo_point"
         }
       }
@@ -73,7 +77,7 @@ const (
           "type": "string",
           "index": "not_analyzed"
         },
-        "departure_date": {
+        "flight_departure_date": {
           "format": "strict_date_optional_time||epoch_millis",
           "type": "date"
         },
@@ -88,7 +92,7 @@ const (
           "type": "string",
           "index": "not_analyzed"
         },
-        "origin_city": {
+        "flight_origin_city": {
           "type": "string",
           "index": "not_analyzed"
         },
@@ -110,15 +114,37 @@ const (
         "stopover": {
           "type": "long"
         },
-        "destination_city": {
+        "flight_destination_city": {
           "type": "string",
           "index": "not_analyzed"
         },
-        "origin_coordinates": {
+        "flight_origin_coordinates": {
           "type": "geo_point"
         },
-        "destination_coordinates": {
+        "flight_destination_coordinates": {
           "type": "geo_point"
+        },
+        "trip_departure_date": {
+          "format": "strict_date_optional_time||epoch_millis",
+          "type": "date"
+        },
+        "trip_origin_city": {
+          "type": "string",
+          "index": "not_analyzed"
+        },
+        "trip_destination_city": {
+          "type": "string",
+          "index": "not_analyzed"
+        },
+        "trip_origin_coordinates": {
+          "type": "geo_point"
+        },
+        "trip_destination_coordinates": {
+          "type": "geo_point"
+        },
+        "trip_main_airline": {
+          "type": "string",
+          "index": "not_analyzed"
         }
       }
     }
@@ -126,37 +152,44 @@ const (
 }`
 )
 
-type ElasticFare struct {
+type ElasticTrip struct {
 	SearchDate             time.Time   `json:"search_date"`
-	Date                   time.Time   `json:"date"`
-	ReturnDate             time.Time   `json:"return_date"`
+	Date                   time.Time   `json:"trip_date"`
+	ReturnDate             time.Time   `json:"trip_return_date"`
 	SearchDateOfTheWeek    string      `json:"search_day_of_the_week"`
-	DayOfTheWeek           string      `json:"day_of_the_week"`
-	ReturnDayOfTheWeek     string      `json:"return_day_of_the_week"`
-	OriginCity             string      `json:"origin_city"`
-	OriginCoordinates      string      `json:"origin_coordinates"`
-	DestinationCity        string      `json:"destination_city"`
-	DestinationCoordinates string      `json:"destination_coordinates"`
+	DayOfTheWeek           string      `json:"trip_day_of_the_week"`
+	ReturnDayOfTheWeek     string      `json:"trip_return_day_of_the_week"`
+	OriginCity             string      `json:"trip_origin_city"`
+	OriginCoordinates      string      `json:"trip_origin_coordinates"`
+	DestinationCity        string      `json:"trip_destination_city"`
+	DestinationCoordinates string      `json:"trip_destination_coordinates"`
 	Provider               string      `json:"provider"`
+	MainAirline            string 	   `json:"trip_main_airline"`
 	FareInfo               string      `json:"fare_info"`
 	PriceInUSD             int         `json:"price_usd"`
 }
 
 type ElasticFlight struct {
-	SearchDate             time.Time  `json:"search_date"`
-	DepartureDate          time.Time  `json:"departure_date"`
-	SearchDateOfTheWeek    string     `json:"search_day_of_the_week"`
-	OriginCity             string     `json:"origin_city"`
-	OriginCoordinates      string     `json:"origin_coordinates"`
-	DestinationCity        string     `json:"destination_city"`
-	DestinationCoordinates string     `json:"destination_coordinates"`
-	Airline                string     `json:"airline"`
-	FlightNumber           string     `json:"flight_number"`
-	Plane                  string     `json:"plane"`
-	FareInfo               string     `json:"fare_info"`
-	Key                    string     `json:"flight_key"`
-	StopOver               int        `json:"stopover"`
-	PriceInUSD             int        `json:"price_usd"`
+	SearchDate                 time.Time  `json:"search_date"`
+	DepartureDate              time.Time  `json:"flight_departure_date"`
+	TripDepartureDate          time.Time  `json:"trip_departure_date"`
+	SearchDateOfTheWeek        string     `json:"search_day_of_the_week"`
+	OriginCity                 string     `json:"flight_origin_city"`
+	OriginCoordinates          string     `json:"flight_origin_coordinates"`
+	DestinationCity            string     `json:"flight_destination_city"`
+	DestinationCoordinates     string     `json:"flight_destination_coordinates"`
+	TripOriginCity             string     `json:"trip_origin_city"`
+	TripDestinationCity        string     `json:"trip_destination_city"`
+	TripOriginCoordinates      string     `json:"trip_origin_coordinates"`
+	TripDestinationCoordinates string     `json:"trip_destination_coordinates"`
+	TripMainAirline            string     `json:"trip_main_airline"`
+	Airline                    string     `json:"airline"`
+	FlightNumber               string     `json:"flight_number"`
+	Plane                      string     `json:"plane"`
+	FareInfo                   string     `json:"fare_info"`
+	Key                        string     `json:"flight_key"`
+	StopOver                   int        `json:"stopover"`
+	PriceInUSD                 int        `json:"price_usd"`
 }
 
 func ReportElasticSearch(host string, port int, index string, fares []agents.Fare) {
@@ -187,7 +220,7 @@ func ReportElasticSearch(host string, port int, index string, fares []agents.Far
 	fmt.Printf("elastic: Indexing %d fares and %d flights...\n", len(elasticFares), len(elasticFlights))
 
 	start := time.Now()
-	BulkIndex(es, index, "fare", elasticFares)
+	BulkIndex(es, index, "trip", elasticFares)
 	BulkIndex(es, index, "flight", elasticFlights)
 	elapsed := time.Since(start)
 
@@ -215,12 +248,15 @@ func BulkIndex(client *elasticsearch.Client, index string, documentType string, 
 	}
 }
 
-func GetElasticDocs(fares []agents.Fare) (elasticFares []interface{}, elasticFlights []interface{}) {
+func GetElasticDocs(fares []agents.Fare) (elasticTrips []interface{}, elasticFlights []interface{}) {
 	for _, fare := range fares {
 		for _, priceInfo := range fare.Prices {
 			if (priceInfo.Currency != "USD") {
 				continue
 			}
+
+			elasticTrip := CreateElasticFare(&fare, &priceInfo)
+			elasticTrips = append(elasticTrips, *elasticTrip)
 
 			for _, itinerary := range fare.Itineraries {
 				for i, flight := range itinerary.Flights {
@@ -229,33 +265,36 @@ func GetElasticDocs(fares []agents.Fare) (elasticFares []interface{}, elasticFli
 						nextFlight = &itinerary.Flights[i + 1]
 					}
 
-					elasticFlight := CreateElasticFlight(&fare, &priceInfo, &flight, nextFlight)
+					elasticFlight := CreateElasticFlight(elasticTrip, &flight, nextFlight)
 					elasticFlights = append(elasticFlights, *elasticFlight)
 				}
 			}
-
-			elasticFare := CreateElasticFare(&fare, &priceInfo)
-			elasticFares = append(elasticFares, *elasticFare)
 		}
 	}
 
-	return elasticFares, elasticFlights
+	return elasticTrips, elasticFlights
 }
 
-func CreateElasticFlight(fare *agents.Fare, priceInfo *agents.PriceInfo, flight *agents.Flight, nextFlight *agents.Flight) *ElasticFlight {
+func CreateElasticFlight(trip *ElasticTrip, flight *agents.Flight, nextFlight *agents.Flight) *ElasticFlight {
 	// The unique key of the flight: departure date + airline IATA code + flight number
 	key := flight.DepartureTime.Format("0102") + flight.Airline + flight.FlightNumber
 
 	elasticFlight := ElasticFlight{
-		Key:                    key,
-		SearchDate:             fare.Date,
-		DepartureDate:          flight.DepartureTime,
-		SearchDateOfTheWeek:    fare.Date.Weekday().String(),
-		Airline:                GetAirlineName(flight.Airline),
-		FlightNumber:           flight.Airline + flight.FlightNumber,
-		Plane:                  flight.Plane,
-		FareInfo:               fare.PrettyString(),
-		PriceInUSD:             int(priceInfo.Price),
+		Key:                    	key,
+		SearchDate:             	trip.SearchDate,
+		DepartureDate:          	flight.DepartureTime,
+		SearchDateOfTheWeek:    	trip.SearchDateOfTheWeek,
+		Airline:                	GetAirlineName(flight.Airline),
+		FlightNumber:           	flight.Airline + flight.FlightNumber,
+		Plane:                  	flight.Plane,
+		FareInfo:               	trip.FareInfo,
+		PriceInUSD:             	trip.PriceInUSD,
+		TripDepartureDate:        	trip.Date,
+		TripOriginCity:        		trip.OriginCity,
+		TripOriginCoordinates:      	trip.OriginCoordinates,
+		TripDestinationCity:        	trip.DestinationCity,
+		TripDestinationCoordinates: 	trip.DestinationCoordinates,
+		TripMainAirline: 		trip.MainAirline,
 	}
 
 	if (nextFlight != nil) {
@@ -274,7 +313,7 @@ func CreateElasticFlight(fare *agents.Fare, priceInfo *agents.PriceInfo, flight 
 	return &elasticFlight
 }
 
-func CreateElasticFare(fare *agents.Fare, priceInfo *agents.PriceInfo) *ElasticFare {
+func CreateElasticFare(fare *agents.Fare, priceInfo *agents.PriceInfo) *ElasticTrip {
 	var trip, returnTrip *agents.Itinerary
 
 	trip = &fare.Itineraries[0]
@@ -282,7 +321,7 @@ func CreateElasticFare(fare *agents.Fare, priceInfo *agents.PriceInfo) *ElasticF
 		returnTrip = &fare.Itineraries[1]
 	}
 
-	elasticFare := ElasticFare{
+	elasticFare := ElasticTrip{
 		SearchDate: 		fare.Date,
 		SearchDateOfTheWeek: 	fare.Date.Weekday().String(),
 		PriceInUSD: 		int(priceInfo.Price),
@@ -292,10 +331,12 @@ func CreateElasticFare(fare *agents.Fare, priceInfo *agents.PriceInfo) *ElasticF
 		DayOfTheWeek:		trip.Flights[0].DepartureTime.Weekday().String(),
 	}
 
+	// Origin
 	city, coordinates := GetAirportInfo(trip.Flights[0].FromAirport)
 	elasticFare.OriginCity = city
 	elasticFare.OriginCoordinates = coordinates
 
+	// Destination
 	var destinationAirport string
 	if (returnTrip != nil) {
 		destinationAirport = returnTrip.Flights[0].FromAirport
@@ -307,10 +348,29 @@ func CreateElasticFare(fare *agents.Fare, priceInfo *agents.PriceInfo) *ElasticF
 	elasticFare.DestinationCity = city
 	elasticFare.DestinationCoordinates = coordinates
 
+	// Return trip info
 	if (returnTrip != nil) {
 		elasticFare.ReturnDate = returnTrip.Flights[0].DepartureTime
 		elasticFare.ReturnDayOfTheWeek = elasticFare.ReturnDate.Weekday().String()
 	}
+
+	// Main Airline
+	mainAirline := ""
+	flightDuration := map[string]float64{}
+	for _, itinerary := range fare.Itineraries {
+		for _, flight := range itinerary.Flights {
+			airline := GetAirlineName(flight.Airline)
+			duration := flight.ArrivalTime.Sub(flight.DepartureTime).Hours()
+
+			flightDuration[airline] += duration
+
+			if (mainAirline != airline && flightDuration[airline] > flightDuration[mainAirline]) {
+				mainAirline = airline
+			}
+		}
+	}
+
+	elasticFare.MainAirline = mainAirline
 
 	return &elasticFare
 }
